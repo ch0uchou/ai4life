e3c2e3e7d12ab480bca46cbf87a8f1f8fa08074b
chouchou <chaudiemhoang2@gmail.com>
2024-03-23 00:52:12 +0700

    Update mainpytorch.py

--- mainpytorch.py
+++ mainpytorch.py
@@ -106,15 +106,12 @@ class LSTM(nn.Module):
     self.hidden_dim = hidden_dim
     self.output_dim = output_dim
     self.lstm = torch.nn.LSTM(input_dim,hidden_dim,layer_num,batch_first=True,bidirectional=True)
-    self.lstm1 = torch.nn.LSTM(input_dim,hidden_dim,layer_num,batch_first=True)
-    print(self.lstm)
-    print(self.lstm1)
-    self.fc = torch.nn.Linear(hidden_dim,output_dim)
+    self.fc = torch.nn.Linear(2*hidden_dim,output_dim)
     self.bn = nn.BatchNorm1d(32)
 
   def forward(self,inputs):
     x = self.bn(inputs)
-    lstm_out,(hn,cn) = self.lstm(x)
+    lstm_out,_ = self.lstm(x)
     out = self.fc(lstm_out[:,-1,:])
     return out
 
