5632ee3c6cd211681cb7af862c8e905d2ee4fe28
chouchou <chaudiemhoang2@gmail.com>
2024-03-23 20:00:06 +0700

    Update yolomodel.py

--- yolomodel.py
+++ yolomodel.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 # Load a model
 model = YOLO('yolov8n-pose.pt')  # load an official model
-model = YOLO('path/to/best.pt')  # load a custom model
+# model = YOLO('path/to/best.pt')  # load a custom model
 
 # Predict with the model
 results = model('4470ea421234a1d0aa58cab42ecd1469.jpg')  # predict on an image
