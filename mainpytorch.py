59b6c440a4bac69b028614c375446d305105d1b8
chouchou <chaudiemhoang2@gmail.com>
2024-03-24 12:11:34 +0700

    Update mainpytorch.py

--- mainpytorch.py
+++ mainpytorch.py
@@ -53,10 +53,10 @@ if args.train:
 elif args.test:
   X_train_path, y_train_path, X_test_path, y_test_path = test(dataset_folder, LABELS)
 else:
-  X_train_path = "dataX_train.txt"
-  y_train_path = "dataY_train.txt"
-  X_test_path = "dataX_test.txt"
-  y_test_path = "dataY_test.txt"
+  X_train_path = "data1X_train.txt"
+  y_train_path = "data1Y_train.txt"
+  X_test_path = "data1X_test.txt"
+  y_test_path = "data1Y_test.txt"
    
 
 n_steps = 32 # 32 timesteps per series
