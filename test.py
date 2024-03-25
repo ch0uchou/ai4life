4e7d1a1a8b2fc3a60b4f47d01566ca856a778afc
chouchou <chaudiemhoang2@gmail.com>
2024-03-25 20:47:46 +0700

    #

--- test.py
+++ test.py
@@ -1,85 +1,80 @@
-# import numpy as np
+import numpy as np
 
-# X_train_path = "/Users/chouchou/chou/github/ai4life/dataX_train.txt"
+X_train_path = "/Users/chouchou/chou/github/ai4life/data1X_train.txt"
 # # X_test_path = file_path_testx
 
-# y_train_path = "/Users/chouchou/chou/github/ai4life/dataY_train.txt"
+y_train_path = "/Users/chouchou/chou/github/ai4life/data1Y_train.txt"
 # # y_test_path = file_path_testy
 
-# n_steps = 32 # 32 timesteps per series
-# n_categories = 22
+n_steps = 32 # 32 timesteps per series
+n_categories = 22
 
 # # Load the networks inputs
 
-# def load_X(X_path, n_steps=32):
-#     file = open(X_path, 'r')
-#     a = [[elem for elem in [
-#             row.strip().split(',') for row in file
-#         ]],]
-#     print(np.a)
-#     # X_ = np.array(
-#     #     [elem for elem in [
-#     #         row.split(',') for row in file
-#     #     ]],
-#     #     dtype=np.float32
-#     # )
-#     # file.close()
-#     # blocks = int(len(X_) / n_steps)
+def load_X(X_path, n_steps=32):
+    file = open(X_path, 'r')
+    X_ = np.array(
+        [elem for elem in [
+            row.split(',') for row in file
+        ]],
+        dtype=np.float32
+    )
+    file.close()
+    blocks = int(len(X_) / n_steps)
 
-#     # X_ = np.array(np.split(X_,blocks))
+    X_ = np.array(np.split(X_,blocks))
 
-#     # return X_
+    return X_
 
-# # Load the networks outputs
-# def load_y(y_path):
-#     file = open(y_path, 'r')
-#     y_ = np.array(
-#         [elem for elem in [
-#             row.replace('  ', ' ').strip().split(' ') for row in file
-#         ]],
-#         dtype=np.int32
-#     ).reshape(-1)
-#     file.close()
+# Load the networks outputs
+def load_y(y_path):
+    file = open(y_path, 'r')
+    y_ = np.array(
+        [elem for elem in [
+            row.replace('  ', ' ').strip().split(' ') for row in file
+        ]],
+        dtype=np.int32
+    ).reshape(-1)
+    file.close()
 
-#     # for 0-based indexing
-#     return y_
+    # for 0-based indexing
+    return y_
 
 
-# X_train = load_X(X_train_path)
-# # X_test = load_X(X_test_path)
+X_train = load_X(X_train_path)
+# X_test = load_X(X_test_path)
 
-# y_train = load_y(y_train_path)
-# # y_test = load_y(y_test_path)
+y_train = load_y(y_train_path)
+# y_test = load_y(y_test_path)
 
-# # print(X_train.shape)
-# # # for x in X_train:
-# # #     # print(x.shape)
-# # #     string = ""
-# # #     string2 = ""
-# # #     string3 = ""
-# # #     for xx in x:
-# # #         xx_add1 = xx + 1
-# # #         xx_minus1 = xx - 1
-# # #         for i in xx:  
-# # #           string += f'{i},'
-# # #         string = string[:-1]+"\n"
-# # #         for i in xx_add1:  
-# # #           string2 += f'{i},'
-# # #         string2 = string2[:-1]+"\n"
-# # #         for i in xx_minus1:  
-# # #           string3 += f'{i},'
-# # #         string3 = string3[:-1]+"\n"
-# # #     with open(f'X.txt', 'a') as file:
-# # #       file.writelines(string)
-# # #       file.writelines(string2)
-# # #       file.writelines(string3)
+print(X_train.shape)
+# for x in X_train:
+#     # print(x.shape)
+#     string = ""
+#     string2 = ""
+#     string3 = ""
+#     for xx in x:
+#         xx_add1 = xx + 1
+#         xx_minus1 = xx + 2
+#         for a in xx:  
+#           string += f'{a},'
+#         string = string[:-1]+"\n"
+#         for a in xx_add1:  
+#           string2 += f'{a},'
+#         string2 = string2[:-1]+"\n"
+#         for a in xx_minus1:  
+#           string3 += f'{a},'
+#         string3 = string3[:-1]+"\n"
+#     with open(f'X.txt', 'a') as file:
+#       file.writelines(string)
+#       file.writelines(string2)
+#       file.writelines(string3)
 
-# # # for x in y_train:
-# # #     for i in x:
-# # #       with open(f'Y.txt', 'a') as file:
-# # #         file.writelines(str(i)+"\n")
-# # #         file.writelines(str(i)+"\n")
-# # #         file.writelines(str(i)+"\n")'
+# for x in y_train:
+#     with open(f'Y.txt', 'a') as file:
+#         file.writelines(str(x)+"\n")
+#         file.writelines(str(x)+"\n")
+#         file.writelines(str(x)+"\n")
 
 
 
@@ -121,3 +116,4 @@
 # #                 with open(file_path_trainx, "a") as file:
 # #                     file.write(read_filesx[t])
 # #                 t += 1
+
