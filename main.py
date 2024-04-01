136c4f0a5b2edeeaa48a8919a99428cc7d954c6b
chouchou <chaudiemhoang2@gmail.com>
2024-04-01 19:31:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -152,8 +152,7 @@ def test(rnn, tensor_X_test, tensor_y_test, n_categories, testfold=None):
   else:
     tensor_X_test = tensor_X_test.to(device)
     output = rnn(tensor_X_test)
-    # guess = LABELS[torch.reshape(output.topk(1)[1],(-1,))[0].item()]
-    print(f'{output.topk(1)}')
+    print(f'{LABELS[output.topk(1)[1].item()]}')
     # print(f'{tensor_y_test}')
 
 if args.plot != None:
